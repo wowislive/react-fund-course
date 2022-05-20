cdba6cd782eac1abd0bc9a578af1336bda190895
Dmitrii Pavlov <wowislive@gmail.com>
2022-05-20 22:22:55 +0300

    add App.css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from "react";
 import ClassCounter from "./components/ClassCounter";
 import Counter from "./components/Counter";
+import "./styles/App.css";
 
 function App() {
   return (
     <div className="App">
-      <Counter />
-      <ClassCounter />
+      <div className="post">
+        <div className="post__content">
+          <strong>1. Javascript</strong>
+          <div>
+            JavaScript is a lightweight interpreted programming language.
+          </div>
+        </div>
+        <div className="post__btns">
+          <button>Delete</button>
+        </div>
+      </div>
     </div>
   );
 }
