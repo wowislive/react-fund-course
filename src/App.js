93b1106d36e806fb33e982a05b8cae59baf3d012
Dmitrii Pavlov <wowislive@gmail.com>
2022-05-23 18:56:34 +0300

    Add PostList.jsx

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import ClassCounter from "./components/ClassCounter";
 import Counter from "./components/Counter";
 import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
 import "./styles/App.css";
 
 function App() {
+  const [posts, setPosts] = useState([
+    { id: 1, title: "Javascript", body: "Description" },
+    { id: 2, title: "Javascript 2", body: "Description" },
+    { id: 3, title: "Javascript 3", body: "Description" },
+  ]);
   return (
     <div className="App">
-      <PostItem post={{ id: 1, title: "Javascript", body: "Description" }} />
+      <PostList posts={posts} title="The list about JS" />
     </div>
   );
 }
