f941eec16da20c59686444a9672934b385735f2e
Dmitrii Pavlov <wowislive@gmail.com>
2022-05-23 15:01:43 +0300

    Add PostItem.jsx

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import React from "react";
 import ClassCounter from "./components/ClassCounter";
 import Counter from "./components/Counter";
+import PostItem from "./components/PostItem";
 import "./styles/App.css";
 
 function App() {
   return (
     <div className="App">
-      <div className="post">
-        <div className="post__content">
-          <strong>1. Javascript</strong>
-          <div>
-            JavaScript is a lightweight interpreted programming language.
-          </div>
-        </div>
-        <div className="post__btns">
-          <button>Delete</button>
-        </div>
-      </div>
+      <PostItem post={{ id: 1, title: "Javascript", body: "Description" }} />
     </div>
   );
 }
