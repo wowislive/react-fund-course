a2eadf9fd1d1c28481c678e966081f3cb04d28c4
Dmitrii Pavlov <wowislive@gmail.com>
2022-06-01 20:02:26 +0300

    Add Loader.jsx

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import PostItem from "./components/PostItem";
 import PostList from "./components/PostList";
 import MyButton from "./components/UI/button/MyButton";
 import MyInput from "./components/UI/input/MyInput";
+import Loader from "./components/UI/loader/Loader";
 import MyModal from "./components/UI/myModal/MyModal";
 import MySelect from "./components/UI/select/MySelect";
 import { usePosts } from "./hooks/usePosts";
@@ -19,6 +20,7 @@ function App() {
   const [filter, setFilter] = useState({ sort: "", query: "" });
   const [modal, setModal] = useState(false);
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+  const [isPostsLoading, setIsPostsLoading] = useState(false);
 
   useEffect(() => {
     fetchPosts();
@@ -30,8 +32,12 @@ function App() {
   };
 
   async function fetchPosts() {
-    const posts = await PostService.getAll();
-    setPosts(posts);
+    setIsPostsLoading(true);
+    setTimeout(async () => {
+      const posts = await PostService.getAll();
+      setPosts(posts);
+      setIsPostsLoading(false);
+    }, 1000);
   }
 
   const removePost = (post) => {
@@ -51,12 +57,19 @@ function App() {
       <hr style={{ margin: "15px 0" }}></hr>
 
       <PostFilter filter={filter} setFilter={setFilter} />
-
-      <PostList
-        remove={removePost}
-        posts={sortedAndSearchedPosts}
-        title="The list about JS"
-      />
+      {isPostsLoading ? (
+        <div
+          style={{ display: "flex", justifyContent: "center", marginTop: 50 }}
+        >
+          <Loader />
+        </div>
+      ) : (
+        <PostList
+          remove={removePost}
+          posts={sortedAndSearchedPosts}
+          title="The list about JS"
+        />
+      )}
     </div>
   );
 }
