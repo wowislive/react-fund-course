7526c6a7fb6683951585228f253c3cd2c1fdb204
Dmitrii Pavlov <wowislive@gmail.com>
2022-06-03 17:55:44 +0300

    Add useFetching

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import MyInput from "./components/UI/input/MyInput";
 import Loader from "./components/UI/loader/Loader";
 import MyModal from "./components/UI/myModal/MyModal";
 import MySelect from "./components/UI/select/MySelect";
+import { useFetching } from "./hooks/useFetching";
 import { usePosts } from "./hooks/usePosts";
 import "./styles/App.css";
 
@@ -20,7 +21,10 @@ function App() {
   const [filter, setFilter] = useState({ sort: "", query: "" });
   const [modal, setModal] = useState(false);
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
-  const [isPostsLoading, setIsPostsLoading] = useState(false);
+  const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
+    const posts = await PostService.getAll();
+    setPosts(posts);
+  });
 
   useEffect(() => {
     fetchPosts();
@@ -30,15 +34,6 @@ function App() {
     setPosts([...posts, newPost]);
     setModal(false);
   };
-
-  async function fetchPosts() {
-    setIsPostsLoading(true);
-    setTimeout(async () => {
-      const posts = await PostService.getAll();
-      setPosts(posts);
-      setIsPostsLoading(false);
-    }, 1000);
-  }
 
   const removePost = (post) => {
     setPosts(posts.filter((p) => p.id !== post.id));
@@ -57,6 +52,7 @@ function App() {
       <hr style={{ margin: "15px 0" }}></hr>
 
       <PostFilter filter={filter} setFilter={setFilter} />
+      {postError && <h1>An error has occurred {postError}</h1>}
       {isPostsLoading ? (
         <div
           style={{ display: "flex", justifyContent: "center", marginTop: 50 }}
